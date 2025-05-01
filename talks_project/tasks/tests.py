f5a94aea9a64ee80e8a42e7b8f1fc6457894a2b6
jsihin <jishin.cheruvattil@slots.com.kw>
2025-05-01 19:11:26 +0530

    django all auth is implemented added tests for socket

--- talks_project/tasks/tests.py
+++ talks_project/tasks/tests.py
@@ -1,46 +1,8 @@
-from channels.testing import ChannelsLiveServerTestCase, WebsocketCommunicator
+from django.test import TestCase
 from django.contrib.auth import get_user_model
-from asgiref.sync import sync_to_async
 from .models import Task, Comment
-from talks_project.talks_project.notification import NotificationConsumer
-import json
 
 User = get_user_model()
-
-class RealTimeTaskUpdateTestCase(ChannelsLiveServerTestCase):
-
-    async def asyncSetUp(self):
-        self.user1 = await sync_to_async(User.objects.create_user)(username='user1', password='password', email='user3@example.com')
-        self.user2 = await sync_to_async(User.objects.create_user)(username='user2', password='password', email='user4@example.com')
-        self.task = await sync_to_async(Task.objects.create)(
-            title="Test Task",
-            description="This is a test task.",
-            priority=Task.HIGH,
-            status=Task.NOT_STARTED
-        )
-        await sync_to_async(self.task.assigned_users.add)(self.user1)
-
-    async def connect_to_websocket(self, user, task_id=None):
-        communicator = WebsocketCommunicator(
-            NotificationConsumer.as_asgi(),
-            f"/ws/notifications/?task_id={task_id}" if task_id else "/ws/notifications/"
-        )
-        connected, _ = await communicator.connect()
-        self.assertTrue(connected)
-        return communicator
-
-    async def test_real_time_task_update(self):
-        communicator_user1 = await self.connect_to_websocket(self.user1, self.task.id)
-        communicator_user2 = await self.connect_to_websocket(self.user2, self.task.id)
-
-        self.task.title = "Updated Test Task"
-        await sync_to_async(self.task.save)()
-
-        response_user2 = await communicator_user2.receive_json_from()
-        self.assertEqual(response_user2['message']['title'], "Updated Test Task")
-
-        await communicator_user1.disconnect()
-        await communicator_user2.disconnect()
 
 class TaskTests(TestCase):
 
